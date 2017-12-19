1275aa5170e9e75010bb536ef7eaa73ae89bba52
zyad224 <zyad224@aucegypt.edu>
2017-12-19 22:44:04 +0200

    fixes

--- app/src/main/java/com/example/zeyad/cameraapplication/MainActivity.java
+++ app/src/main/java/com/example/zeyad/cameraapplication/MainActivity.java
@@ -1,51 +1,93 @@
 package com.example.zeyad.cameraapplication;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.arch.persistence.room.Room;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
+
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import com.example.zeyad.cameraapplication.database.*;
 
+import pl.aprilapps.easyphotopicker.DefaultCallback;
 import pl.aprilapps.easyphotopicker.EasyImage;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final int REQUEST_READ_EXTERNAL_STORAGE = 2987;
+    private static final int REQUEST_WRITE_EXTERNAL_STORAGE = 7829;
+    private static final String TAG = "MainActivity";
+    private static List<ImageElement> myPictureList = new ArrayList<>();
+    private RecyclerView.Adapter  mAdapter;
+    private RecyclerView mRecyclerView;
+    private Activity activity;
     private static AppDatabase db;
     private static boolean databaseLoaded=false;
-    private Activity activity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         activity= this;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        mRecyclerView = (RecyclerView) findViewById(R.id.grid_recycler_view);
+        int numberOfColumns = 4;
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        mAdapter= new MyAdapter(myPictureList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        if (myPictureList==null || myPictureList.size()==0) {
+            initData();
+        }
+
+        // required by Android 6.0 +
+        checkPermissions(getApplicationContext());
+
+        initEasyImage();
+
+
         FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fabGallery = (FloatingActionButton) findViewById(R.id.fab_gallery);
+        FloatingActionButton fabCamera = (FloatingActionButton) findViewById(R.id.fab_camera);
+
+        fabGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, BrowseImageActivity.class);
-                startActivity(intent);
+                EasyImage.openGallery(getActivity(), 0);
             }
         });
 
+        fabCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EasyImage.openCamera(getActivity(), 0);
+            }
+        });
         fab2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,15 +110,134 @@ public class MainActivity extends AppCompatActivity {
         new InsertIntoDatabaseTask().execute();
         // search the database
         new SearchDatabaseTask().execute();*/
+    }
 
 
-        FloatingActionButton fabCamera = (FloatingActionButton) findViewById(R.id.fab_camera);
-        fabCamera.setOnClickListener(new View.OnClickListener() {
+    private void initEasyImage() {
+        EasyImage.configuration(this)
+                .setImagesFolderName("EasyImage sample")
+                .setCopyTakenPhotosToPublicGalleryAppFolder(false)
+                .setCopyPickedImagesToPublicGalleryAppFolder(false)
+                .setAllowMultiplePickInGallery(true);
+    }
+
+    private void initData() {
+        myPictureList.add(new ImageElement(R.drawable.joe1));
+        myPictureList.add(new ImageElement(R.drawable.joe2));
+        myPictureList.add(new ImageElement(R.drawable.joe3));
+        myPictureList.add(new ImageElement(R.drawable.joe1));
+        myPictureList.add(new ImageElement(R.drawable.joe2));
+        myPictureList.add(new ImageElement(R.drawable.joe3));
+        myPictureList.add(new ImageElement(R.drawable.joe1));
+        myPictureList.add(new ImageElement(R.drawable.joe2));
+        myPictureList.add(new ImageElement(R.drawable.joe3));
+        myPictureList.add(new ImageElement(R.drawable.joe1));
+        myPictureList.add(new ImageElement(R.drawable.joe2));
+        myPictureList.add(new ImageElement(R.drawable.joe3));
+        myPictureList.add(new ImageElement(R.drawable.joe1));
+        myPictureList.add(new ImageElement(R.drawable.joe2));
+        myPictureList.add(new ImageElement(R.drawable.joe3));
+    }
+
+    private void checkPermissions(final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    android.support.v7.app.AlertDialog.Builder alertBuilder = new android.support.v7.app.AlertDialog.Builder(context);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle("Permission necessary");
+                    alertBuilder.setMessage("External storage permission is necessary");
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_EXTERNAL_STORAGE);
+                        }
+                    });
+                    android.support.v7.app.AlertDialog alert = alertBuilder.create();
+                    alert.show();
+
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_EXTERNAL_STORAGE);
+                }
+
+            }
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    android.support.v7.app.AlertDialog.Builder alertBuilder = new android.support.v7.app.AlertDialog.Builder(context);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle("Permission necessary");
+                    alertBuilder.setMessage("Writing external storage permission is necessary");
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE);
+                        }
+                    });
+                    android.support.v7.app.AlertDialog alert = alertBuilder.create();
+                    alert.show();
+
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE);
+                }
+
+            }
+
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
             @Override
-            public void onClick(View view) {
-                EasyImage.openCamera(getActivity(), 0);
+            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
+                //Some error handling
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onImagesPicked(List<File> imageFiles, EasyImage.ImageSource source, int type) {
+                onPhotosReturned(imageFiles);
+            }
+
+            @Override
+            public void onCanceled(EasyImage.ImageSource source, int type) {
+
             }
         });
+    }
+
+    /**
+     * add the selected images to the grid
+     * @param returnedPhotos
+     */
+    private void onPhotosReturned(List<File> returnedPhotos) {
+        myPictureList.addAll(getImageElements(returnedPhotos));
+        // we tell the adapter that the data is changed and hence the grid needs
+        // refreshing
+        mAdapter.notifyDataSetChanged();
+        mRecyclerView.scrollToPosition(returnedPhotos.size() - 1);
+    }
+
+    /**
+     * given a list of photos, it creates a list of myElements
+     * @param returnedPhotos
+     * @return
+     */
+    private List<ImageElement> getImageElements(List<File> returnedPhotos) {
+        List<ImageElement> imageElementList= new ArrayList<>();
+        for (File file: returnedPhotos){
+            ImageElement element= new ImageElement(file);
+            imageElementList.add(element);
+        }
+        return imageElementList;
+    }
+
+    public Activity getActivity() {
+        return activity;
     }
 
     @Override
@@ -108,8 +269,8 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
             Location location = new Location(53.3829700, -1.4659000, 20);
             db.imageDao().insertLocation(location);
-           // Image image= new Image(getApplicationContext(), R.drawable.joe1, "joe1", "this is Joe 1", location.getId());
-           // db.imageDao().insertImage(image);
+            // Image image= new Image(getApplicationContext(), R.drawable.joe1, "joe1", "this is Joe 1", location.getId());
+            // db.imageDao().insertImage(image);
 
 
             return null;
@@ -143,6 +304,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public Activity getActivity(){return activity;}
+
 
 }
