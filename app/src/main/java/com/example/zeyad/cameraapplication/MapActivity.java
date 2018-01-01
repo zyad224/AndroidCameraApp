9d534fe64c8150c4243a5bde1c8c1e59fe3269a5
zyad224 <zyad224@aucegypt.edu>
2018-01-02 01:31:30 +0200

    sizing images

--- app/src/main/java/com/example/zeyad/cameraapplication/MapActivity.java
+++ app/src/main/java/com/example/zeyad/cameraapplication/MapActivity.java
@@ -112,7 +112,8 @@ public class MapActivity extends AppCompatActivity  implements GoogleMap.OnMarke
             for(Wrapper wo: w) {
 
                m=   mMap.addMarker(new MarkerOptions().position(wo.getPositionOnMap()).title(wo.getImage().getTitle())
-                       .icon(BitmapDescriptorFactory.fromBitmap(myBitmap =decodeSampledBitmapFromResource(wo.getImage().getImagepath(), 50, 50))));
+                       .icon(BitmapDescriptorFactory.fromBitmap(myBitmap = MyAdapter.decodeSampledBitmapFromResource
+                               (wo.getImage().getImagepath(), 50, 50))));
                addListenerstoMarkers();
             }
 
@@ -134,43 +135,6 @@ public class MapActivity extends AppCompatActivity  implements GoogleMap.OnMarke
         }
     };
 
-    public static Bitmap decodeSampledBitmapFromResource(String filePath, int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(filePath, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(filePath, options);
-    }
-
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
 
     public void addListenerstoMarkers(){
 
@@ -182,7 +146,10 @@ public class MapActivity extends AppCompatActivity  implements GoogleMap.OnMarke
         LatLng markerPosition= marker.getPosition();
         String markerTitle=marker.getTitle();
 
+
         return false;
     }
+
+
 
 }
